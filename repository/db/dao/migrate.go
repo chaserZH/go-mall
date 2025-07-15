5f9fc13de4d723acfbf4179be81a1a3aca3c9b71
chaserZH <zhanghao2959@gmail.com>
2025-07-15 11:39:54 +0800

    go-mall-study

--- repository/db/dao/migrate.go
+++ repository/db/dao/migrate.go
@@ -1,8 +1,15 @@
 package dao
 
+import "go-mall/repository/db/model"
+
 func migrate() (err error) {
-	err = _db.Set("gorm:table_options", "charset=utf8mb4").
-		AutoMigrate()
+	err = db.Set("gorm:table_options", "charset=utf8mb4").
+		AutoMigrate(&model.User{}, &model.Favorite{},
+			&model.Order{}, &model.Admin{}, &model.Address{},
+			&model.Cart{}, &model.Category{}, &model.Carousel{},
+			&model.Notice{}, &model.Notice{}, &model.Product{},
+			&model.ProductImg{}, &model.SkillProduct{},
+		)
 
 	return
 }
